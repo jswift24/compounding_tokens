1184aac66b2198428ca02400a3adcc524350b756
robin0319 <89219896+robin0319@users.noreply.github.com>
2022-03-08 15:40:16 +0200

    final

--- apis/compoundingService.js
+++ apis/compoundingService.js
@@ -76,13 +76,13 @@ const getTokenBalances = async (address, block_num, tokenAddress) => {
     const options = { chain: "eth", address: address, to_block: block_num };
     const balances = await Moralis.Web3API.account.getTokenBalances(options);
     let balance;
-    for ( var i = 0; i < balances.length; i++){
-        if ( balances[i].token_address == tokenAddress){
+    for (var i = 0; i < balances.length; i++) {
+        if (balances[i].token_address == tokenAddress) {
             balance = balances[i].balance;
             break;
         }
     }
-    
+
     if (balance) {
         return balance;
     } else {
@@ -92,14 +92,14 @@ const getTokenBalances = async (address, block_num, tokenAddress) => {
 }
 
 const getBalance = async (walletAddress, tokenAddress) => {
-    
+
     const contract = new Web3Client.eth.Contract(minABI, tokenAddress);
 
     let balance;
 
-    await contract.methods.balanceOf(walletAddress).call().then( (res) => {
+    await contract.methods.balanceOf(walletAddress).call().then((res) => {
         balance = res;
-    }).catch((err) =>{
+    }).catch((err) => {
         balance = 0;
     })
     return balance;
@@ -113,29 +113,57 @@ const getCompundingToken = () => {
                 let compoundingTokenList = [];
                 let lastBlockNumber;
                 await Web3Client.eth.getBlockNumber()
-                .then( (res) => {
-                    lastBlockNumber = res;
+                    .then((res) => {
+                        lastBlockNumber = res;
+                    }).catch((err) => {
+                        console.log(err);
+                    });
+                let protocols;
+                URL = "https://openapi.debank.com/v1/protocol/list";
+                await axios.get(URL).then(({data}) => {
+                    protocols = data;
                 }).catch((err) => {
                     console.log(err);
                 });
-                while(1) {
-                    cnt++;
-                    URL = "https://etherscan.io/accounts/" + cnt.toString();
-                    var midAddress = [];
-                    await axios.get(URL).then(({ data }) => {
-                        const $ = cheerio.load(data); // Initialize cheerio 
-                        const tempAddresses = extractLinks($);
-                        for (var j = 0; j < tempAddresses.length; j++) {
-                            midAddress.push(tempAddresses[j]);
+                for (var i = 0; i < 35; i++) {
+                    let protocolUrl = "https://api.debank.com/project/portfolios/user_list?id=" + protocols[i].id;
+                    while (1) {
+                        let address = [];
+                        let sit = 0;
+                        await axios.get(protocolUrl).then(({ data }) => {
+                            if (data.data.user_list.length > 1) {
+                                sit = 1;
+                                for (var k = 0; k < data.data.user_list.length; k++) {
+                                    if (data.data.user_list[k].user_addr) {
+                                        address.push(data.data.user_list[k].user_addr);
+                                        sit = 1;
+                                    } else {
+                                        sit = 0;
+                                        break;
+                                    }
+                                }
+                            } else {
+                                sit = 0;
+                            }
+
+                        });
+                        let z = 0;
+                        for (var x = 0; x < address.length; x++) {
+
+                            let isContract = await Web3Client.eth.getCode(address[x])
+                            if (isContract.length == 2) {
+                                addresses.push(address[x]);
+                                z++;
+                            }
+                            if (z >= 5) {
+                                break;
+                            }
                         }
-                    });
-                    for (var i = 0; i < midAddress.length; i++) {
-                        let isContract = await Web3Client.eth.getCode(midAddress[i]);
-                        if (isContract.length == 2){
-                            addresses.push(midAddress[i])
-                        }
+                        break;
                     }
-                    if (addresses.length >= 100) break;
+                    if (addresses.length >= 100) {
+                        break;
+                    }
                 }
                 for (var i = 0; i < 100; i++) {
                     let tempTransactionList = await getTokenTransfers(addresses[i]);
@@ -144,16 +172,16 @@ const getCompundingToken = () => {
                         let tempBalance = 0;
                         if (tempTransactionList[j][0].address == addresses[i]) {
                             continue;
-                        } 
+                        }
                         for (var k = 0; k < tempTransactionList[j].length; k++) {
                             if (tempTransactionList[j][k].to_address == addresses[i]) {
-                                tempBalance += tempTransactionList[j][k].value/100000000;
+                                tempBalance += tempTransactionList[j][k].value / 100000000;
                             } else {
-                                tempBalance -= tempTransactionList[j][k].value/100000000;
+                                tempBalance -= tempTransactionList[j][k].value / 100000000;
                             }
                         }
                         let currentBalance = await getBalance(addresses[i], tempTransactionList[j][0].address)
-                        if (tempBalance == currentBalance/100000000){
+                        if (tempBalance == currentBalance / 100000000) {
                             continue;
                         } else {
                             let flag = 0;
@@ -167,39 +195,38 @@ const getCompundingToken = () => {
                                 compoundingTokenList.push(tempTransactionList[j][0].address);
 
                                 let blockObj = {
-                                    coinAddress : "",
-                                    walletAddress : "",
-                                    blocks : [
-                                        
+                                    coinAddress: "",
+                                    walletAddress: "",
+                                    blocks: [
+
                                     ]
                                 };
                                 if (tempTransactionList[j].length == 1) {
-                                    let balance1 = await getTokenBalances(addresses[i], tempTransactionList[j][0].block_number * 1 , tempTransactionList[j][0].address)
-                                    let last = await getTokenBalances(addresses[i], lastBlockNumber , tempTransactionList[j][0].address)
-                                    if ( balance1 != last ){
-                                        console.log(tempTransactionList[j][0])
-                                        for( var y = 1; y < 500; y ++){
-                                            let balance2 = await getTokenBalances(addresses[i], tempTransactionList[j][0].block_number * 1 + y  , tempTransactionList[j][0].address)
+                                    let balance1 = await getTokenBalances(addresses[i], tempTransactionList[j][0].block_number * 1, tempTransactionList[j][0].address)
+                                    let last = await getTokenBalances(addresses[i], lastBlockNumber, tempTransactionList[j][0].address)
+                                    if (balance1 != last) {
+                                        for (var y = 1; y < 500; y++) {
+                                            let balance2 = await getTokenBalances(addresses[i], tempTransactionList[j][0].block_number * 1 + y, tempTransactionList[j][0].address)
                                             if (balance1 != balance2 && (balance1 != 0 || balance2 != 0)) {
                                                 blockObj = {
-                                                    coinAddress : tempTransactionList[j][0].address,
-                                                    walletAddress : addresses[i],
-                                                    blocks : [
+                                                    coinAddress: tempTransactionList[j][0].address,
+                                                    walletAddress: addresses[i],
+                                                    blocks: [
                                                         {
-                                                            blockNumber : tempTransactionList[j][0].block_number * 1 + y-1,
-                                                            balance : balance1
+                                                            blockNumber: tempTransactionList[j][0].block_number * 1 + y - 1,
+                                                            balance: balance1
                                                         },
                                                         {
-                                                            blockNumber : tempTransactionList[j][0].block_number * 1 + y,
-                                                            balance : balance2
+                                                            blockNumber: tempTransactionList[j][0].block_number * 1 + y,
+                                                            balance: balance2
                                                         }
-                                                       
+
                                                     ]
                                                 }
                                                 block.push(blockObj)
                                                 break;
                                             }
-                                            else{
+                                            else {
                                                 balance1 = balance2
                                             }
                                         }
@@ -210,25 +237,23 @@ const getCompundingToken = () => {
                             }
                         }
                     }
-                    if(block.length > 0) {
-                        fs.readFile('./CompoundingList.json', 'utf-8', function(err, data) {
+                    if (block.length > 0) {
+                        fs.readFile('./CompoundingList.json', 'utf-8', function (err, data) {
                             if (err) throw err
-                        
+
                             var arrayOfObjects = JSON.parse(data)
                             arrayOfObjects.push(block)
-                        
-                            fs.writeFile('./CompoundingList.json', JSON.stringify(arrayOfObjects), 'utf-8', function(err) {
+
+                            fs.writeFile('./CompoundingList.json', JSON.stringify(arrayOfObjects), 'utf-8', function (err) {
                                 if (err) throw err
-                                console.log('Done!')
                             })
                         })
                     }
                 }
-                console.log('Finished!')
-                return resolve({ success : true , fileName : "CompoundingList.json" })
+                return resolve({ success: true, fileName: "CompoundingList.json" })
             } catch (error) {
                 console.log(error)
-                return resolve({ success : false })
+                return resolve({ success: false })
             }
         }
         main();
